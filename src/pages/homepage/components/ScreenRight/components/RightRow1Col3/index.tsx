1a87df922c408d01b41f2ae911519bf272916d16
tunshiyu <552134826@qq.com>
2020-05-21 18:57:14 +0800

    chore: 修改命名

--- src/pages/homepage/components/ScreenRight/components/RightRow1Col3/index.tsx
+++ src/pages/homepage/components/ScreenRight/components/RightRow1Col3/index.tsx
@@ -4,24 +4,18 @@
  * @作者: 阮旭松
  * @Date: 2020-05-14 15:54:36
  * @LastEditors: 于效仟
- * @LastEditTime: 2020-05-21 15:26:10
+ * @LastEditTime: 2020-05-21 18:56:32
  */
 import React from 'react';
 import { ComBlock, ComCard, IconsScoreChart } from '@td-design/charts';
 import styles from './index.module.less';
-import { standardsDatas, scoreDatas } from '@/pages/homepage/data';
+import { standardsData, scoreData } from '@/pages/homepage/data';
 
 const LeftRow2Col2 = () => {
   return (
     <ComBlock className={styles.block}>
       <ComCard title="评分" headerStyle={{ height: 16 }}>
-        <IconsScoreChart
-          standardsDatas={standardsDatas}
-          scoreDatas={scoreDatas}
-          size={16}
-          backIcon={require(`@/assets/materielGray.png`)}
-          frontIcon={require(`@/assets/materiel.png`)}
-        />
+        <IconsScoreChart standards={standardsData} scores={scoreData} />
       </ComCard>
     </ComBlock>
   );
