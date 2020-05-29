ac303f44253cafd35b17d6271bf9d9f3cf0367fd
rxs-michael <ruanxusong@thundersdata.com>
2020-05-29 17:26:44 +0800

    fix: 只保留大屏结构

--- config/config.ts
+++ config/config.ts
@@ -3,29 +3,22 @@
  * @公司: thundersdata
  * @作者: 于效仟
  * @Date: 2020-05-16 14:33:48
- * @LastEditors: 于效仟
- * @LastEditTime: 2020-05-21 15:44:01
+ * @LastEditors: 阮旭松
+ * @LastEditTime: 2020-05-29 17:25:05
  */
 
 import { defineConfig } from 'umi';
-import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
 import routeConfig from './routeConfig';
 
 export default defineConfig({
-  dynamicImport: {
-    loading: '@/components/Loading.tsx',
-  },
   hash: true,
   outputPath: 'build',
-  routes: routeConfig,
-  polyfill: {
-    imports: ['core-js/stable'],
-  },
   esbuild: {},
   metas: [
     { name: 'msapplication-TileColor', content: '#da532c' },
     { name: 'theme-color', content: '#ffffff' },
   ],
+  routes: routeConfig,
   links: [
     {
       rel: 'apple-touch-icon',
@@ -46,12 +39,5 @@ export default defineConfig({
     },
     { rel: 'manifest', href: '/site.webmanifest' },
     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-    {
-      rel: 'stylesheet',
-      href: '//at.alicdn.com/t/font_1509107_vaarx0n4zz.css',
-    },
   ],
-  chainWebpack(config) {
-    config.plugin('dayjs').use(AntdDayjsWebpackPlugin);
-  },
 });
