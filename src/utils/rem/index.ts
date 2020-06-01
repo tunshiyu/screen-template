543a3c3821bdac3d5a521c0cb4456d21eb63fad2
tunshiyu <552134826@qq.com>
2020-06-01 17:22:02 +0800

    fix: 删除无用及使用document.ejs

--- src/utils/rem/index.ts
+++ src/utils/rem/index.ts
@@ -4,30 +4,14 @@
  * @作者: 于效仟
  * @Date: 2020-05-30 16:02:10
  * @LastEditors: 于效仟
- * @LastEditTime: 2020-06-01 15:20:09
+ * @LastEditTime: 2020-06-01 17:20:35
  */
 
 export const RootValue = 10;
-export const rem = (px: number) => `${px / RootValue}rem`;
 
-export function remFunc() {
-  // rem适配
-  const styleN = document.createElement('style');
-  styleN.innerHTML = 'html{font-size:' + RootValue + 'px}';
-  styleN.innerHTML += ` @media screen and (min-width: 1280px) {
-    html {
-      font-size: ${RootValue}px;
-    }
-  }
-  @media screen and (max-width: 1280px) {
-    html {
-      font-size: 8px;
-    }
-  }
-  @media screen and (max-width: 960px) {
-    html {
-      font-size: 6px;
-    }
-  }`;
-  document.head.appendChild(styleN);
-}
+/**
+ * @功能描述: 组件行内样式或者特殊属性需要rem时，可使用该函数
+ * @参数: UI图px
+ * @返回值: rem
+ */
+export const rem = (px: number) => `${px / RootValue}rem`;
